e7a246480b092822f08de23745624cd11c970e58
Ekaterina Marinets <ekaterina.marinets@gmail.com>
2022-11-22 02:53:28 +0200

    Create and render markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+console.log(createImgCardsMarkup(galleryItems));
 
-console.log(galleryItems);
+const imgGalleryObject = document.querySelector('.gallery');
+const markupGallery = createImgCardsMarkup(galleryItems);
+imgGalleryObject.insertAdjacentHTML("beforeend", markupGallery);
+// imgGalleryObject.addEventListener('click', onClickImg);
+
+function createImgCardsMarkup(galleryItems) {
+   return galleryItems
+      .map(({ preview, original, description }) => {
+         return `<div class ="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img class="gallery__image" 
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"/>
+        </a>
+        </div>`;
+      }).join("");
+}
